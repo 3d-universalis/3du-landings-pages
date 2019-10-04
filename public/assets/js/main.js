23848314973b6256e8aaf214be5d3bd74cc900b3
Ricky Notaro-Garcia <me@rickynotaro.com>
2019-10-04 09:48:31 -0400

    Updated form bug

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -167,7 +167,7 @@ $(".goToContact").click(function() {
     var FD = new FormData(form);
 
     // Transform formdata to an Array
-    var formObject = { name: FD.get("name"), phone: FD.get("name"), email: FD.get("email"), message: FD.get("message"), origin: "LandingPages" };
+    var formObject = { name: FD.get("name"), phone: FD.get("phone"), email: FD.get("email"), message: FD.get("message"), origin: "LandingPages" };
 
     var alertField = document.getElementById("contact-status");
 
