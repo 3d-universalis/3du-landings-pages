0a05a152defe9d8ab56edc682b56d1e7f70c8156
Ricky Notaro-Garcia <me@rickynotaro.com>
2019-10-16 18:22:54 -0400

    PrintingProcess module

---
+++ src/base-landing/fr/PrintingProcess.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class PrintingProcess extends Component{
+    render(){
+
+        let printingProcessURLs =
+        [
+            {name: 'Plastiques', youtubeURL: 'https://www.youtube.com/embed/FqQAjkZOBeY' },
+            {name: 'Métalliques', youtubeURL: 'https://www.youtube.com/embed/te9OaSZ0kf8' },
+        ];
+        
+        let processList = printingProcessURLs.map((val, i)=> {
+            return(
+                <div className="single-feature col-lg-6 col-md-6 col-12 mb--50" key={i}>
+                    <span className="">{val.name}</span>
+                    <div className="content fix">
+                    <iframe width="100%" height="315" src={val.youtubeURL} frameBorder="0" allowFullScreen></iframe>
+                    </div>
+                </div>
+            )
+        });
+       
+
+        return(
+            <div>
+                <div className="feature-section section bg-gray pt--50 pb--100">
+                    <div className="container">
+                        <div className="row">
+                            <div className="section-title text-center col-12 mb--70">
+                                <h1>Processus d'impression</h1>
+                            </div>
+                            {processList}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PrintingProcess;
