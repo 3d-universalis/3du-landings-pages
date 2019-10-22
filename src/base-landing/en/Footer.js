95484d87622f9e1d2d3213afcef436edb52f190a
Ricky Notaro-Garcia <me@rickynotaro.com>
2019-10-22 12:16:53 -0400

    Misc changes

--- src/base-landing/en/Footer.js
+++ src/base-landing/en/Footer.js
@@ -16,7 +16,7 @@ class Footer extends Component {
                                         <a href="https://www.linkedin.com/company/18952599"><i className="zmdi zmdi-linkedin"></i></a>
                                     </div>
                                     <div className="footer-text text-center">
-                                        <span>Copyright © 2019 - <a href="https://3Duniversalis.com">3D Universalis</a></span>
+                                        <span>Copyright © 2019 - <a href="https://3Duniversalis.com">3D Universalis</a><br/>info@3duniversalis.com</span>
                                     </div>
                                 </div>
                             </div>
